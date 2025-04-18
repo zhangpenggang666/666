e4f5f519e50592c0816c3a616cc8f9dba3b2e7e6
zhangpenggang666 <zhangpenggang0705@gmail.com>
2025-04-18 13:14:41 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 function sayHello() {
     alert("为我们伊一点赞一次");
     alert("点赞总次数为'点赞数'次");
-     let likes = 0;
-
-    function like() {
-      likes++;
-      document.getElementById("count").innerText = "点赞数：" + likes;
-    }
-    <button onclick="like()">👍 点赞</button>
-  <div id="count">点赞数：0</div>
   }
   
